8572e0ad28b8e84417104525af47d1c67ceab7ad
Karl <karl@kejk.tech>
2022-07-31 12:48:41 +0100

    fix: text button spacing and state

--- components/TextButton.jsx
+++ components/TextButton.jsx
@@ -8,7 +8,7 @@ const TextButton = ({
   return (
     <button
       className={classNames(
-        `text-${textColor} dark:text-${darkTextColor} rounded-md flex items-center justify-center space-x-2 py-2 px-4 text-sm font-medium transition ease-in-out hover:border-teal-500 w-max md:text-base`
+        `text-${textColor} dark:text-${darkTextColor} flex items-center justify-center space-x-2 text-sm font-medium transition ease-in-out hover:text-teal-500 w-max md:text-base`
       )}
     >
       {children}
