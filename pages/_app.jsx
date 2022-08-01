7aabed84db4b8d43bd9027a074f892090d2c3aa8
Karl Koch <karl@kejk.tech>
2022-08-01 10:27:22 +0100

    fix: adds Plausible

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,11 +1,14 @@
 import Layout from "../components/Layout";
 import "../styles/globals.css";
+import PlausibleProvider from "next-plausible";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <PlausibleProvider domain="kejk.tech">
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </PlausibleProvider>
   );
 }
 
