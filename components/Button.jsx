0d117df8e832ff737832a8bd55f826bed54450a4
Karl <karl@kejk.tech>
2022-08-03 08:07:33 +0100

    fixes: button

--- components/Button.jsx
+++ components/Button.jsx
@@ -11,7 +11,7 @@ const Button = ({
   href,
 }) => {
   return (
-  <a href=${href} className={classNames(
+  <a href={href} className={classNames(
         `bg-${bgColor} text-${textColor} dark:bg-${darkBgColor} dark:text-${darkTextColor} rounded-md border border-${borderColor} dark:border-${darkBorderColor} flex w-full items-center justify-center space-x-2 px-4 py-2 text-sm font-medium transition ease-in-out hover:border-teal-500 md:w-max md:text-base`
       )}>
     <button>
