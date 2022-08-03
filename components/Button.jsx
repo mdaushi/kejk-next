bddb3097d8e1a5fc59abdf40914cdd5a21a02e65
Karl <karl@kejk.tech>
2022-08-03 08:12:23 +0100

    fix: spacing

--- components/Button.jsx
+++ components/Button.jsx
@@ -14,7 +14,7 @@ const Button = ({
   <a href={href} className={classNames(
         `bg-${bgColor} text-${textColor} dark:bg-${darkBgColor} dark:text-${darkTextColor} rounded-md border border-${borderColor} dark:border-${darkBorderColor} flex w-full items-center justify-center space-x-2 px-4 py-2 text-sm font-medium transition ease-in-out hover:border-teal-500 md:w-max md:text-base`
       )}>
-    <button>
+    <button className="flex w-full items-center justify-center space-x-2">
       {children}
     </button>
     </a>
