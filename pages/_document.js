9d0ac2d49a91c614ea3561c0a4677ae12eed8b40
Karl <karl@kejk.tech>
2022-08-07 08:09:01 +0100

    fix: missing closing tags

--- pages/_document.js
+++ pages/_document.js
@@ -9,9 +9,9 @@ export default function Document() {
             as="style"
             href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap"
           />
-      <link rel="preconnect" href="https://fonts.googleapis.com"> 
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin> 
-<link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet" media="print" onLoad="this.media='all'">
+      <link rel="preconnect" href="https://fonts.googleapis.com" /> 
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin /> 
+<link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet" media="print" onLoad="this.media='all'" />
       </Head>
       <body>
         <Main />
