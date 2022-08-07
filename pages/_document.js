2ef5aa69ea731f9c7c10b817fae89b14f89ca0a3
Karl <karl@kejk.tech>
2022-08-07 08:07:08 +0100

    feat: adds Inter as default font

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,16 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+      <link
+            rel="preload"
+            as="style"
+            href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap"
+          />
+      <link rel="preconnect" href="https://fonts.googleapis.com"> 
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin> 
+<link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet" media="print" onLoad="this.media='all'">
+      </Head>
       <body>
         <Main />
         <NextScript />
