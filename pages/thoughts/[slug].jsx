20d4d7b51a222c411a9dd8833c5168b3ab4c2a60
Karl <karl@kejk.tech>
2022-08-08 07:13:11 +0100

    fix: link wrapping on text button

--- pages/thoughts/[slug].jsx
+++ pages/thoughts/[slug].jsx
@@ -115,8 +115,8 @@ export default function Post({ post }) {
             <div className="group mb-8 flex w-full justify-start">
             <a>
               <TextButton textColor="black" darkTextColor="white">
+              <Link href={"/writing"}>
                 <ArrowSmLeftIcon className="h-6 w-6 flex-shrink-0 text-neutral-500 group-hover:text-teal-500 dark:text-neutral-400" />
-                <Link href={"/writing"}>
                   All thoughts
                 </Link>
               </TextButton>
