3077f0c8a7804003269c9d0d26175bb11108fa1b
Karl Koch <karl@kejk.tech>
2022-08-19 21:32:39 +0100

    fix: adds download tracking to bottom button

--- pages/lazyPDF.jsx
+++ pages/lazyPDF.jsx
@@ -191,11 +191,17 @@ const LazyPDF = ({ lazyPDF, stats }) => {
         </div>
         <div dangerouslySetInnerHTML={{ __html: lazyPDF.content }} />
         <div className="mt-12 inline-flex w-full justify-center">
-          <a href={downloadURL} download className="unstyled">
+          <a
+            href={downloadURL}
+            download
+            className="unstyled"
+            onClick={updateDownloads}
+          >
             <button
               className={classNames(
                 `mb-4 flex items-center justify-center space-x-2 rounded-md border border-neutral-200 bg-neutral-100 py-2 px-4 text-sm font-medium text-black transition ease-in-out hover:border-teal-500 dark:border-neutral-700 dark:bg-neutral-800 dark:text-white md:w-max md:text-base`
               )}
+              id="downloads"
             >
               <ArrowDownIcon className="mr-2 h-6 w-6 flex-shrink-0 text-neutral-500 dark:text-neutral-400" />
               Download
