e48d15149189f4de3b09ce8e83b513f1e003feb2
Karl <karl@kejk.tech>
2022-09-06 06:19:25 +0100

    feat: new sans serif

--- pages/_document.js
+++ pages/_document.js
@@ -13,12 +13,12 @@ export default function Document() {
           rel={"preload"}
           as={"style"}
           href={
-            "https://fonts.googleapis.com/css2?family=Inter:ital,wght@0,400;0,500;0,600;0,700;0,900;1,400;1,500;1,600;1,700;1,900&JetBrains+Mono:ital@0;1&display=swap"
+            "https://fonts.googleapis.com/css2?family=Archivo:ital,wght@0,400;0,500;0,600;0,700;0,800;0,900;1,400;1,500;1,600;1,700;1,800;1,900&JetBrains+Mono:ital@0;1&display=swap"
           }
         />
         <link
           href={
-            "https://fonts.googleapis.com/css2?family=Inter:ital,wght@0,400;0,500;0,600;0,700;0,900;1,400;1,500;1,600;1,700;1,900&JetBrains+Mono:ital@0;1&display=swap"
+            "https://fonts.googleapis.com/css2?family=Archivo:ital,wght@0,400;0,500;0,600;0,700;0,800;0,900;1,400;1,500;1,600;1,700;1,800;1,900&JetBrains+Mono:ital@0;1&display=swap"
           }
           rel={"stylesheet"}
           media={"print"}
@@ -28,7 +28,7 @@ export default function Document() {
           <link
             rel={"stylesheet"}
             href={
-              "https://fonts.googleapis.com/css2?family=Inter:ital,wght@0,400;0,500;0,600;0,700;0,900;1,400;1,500;1,600;1,700;1,900&JetBrains+Mono:ital@0;1&display=swap"
+          "https://fonts.googleapis.com/css2?family=Archivo:ital,wght@0,400;0,500;0,600;0,700;0,800;0,900;1,400;1,500;1,600;1,700;1,800;1,900&JetBrains+Mono:ital@0;1&display=swap"
             }
           />
         </noscript>
