30e7365613e0070c5ad7abf25c6c8ca7b22e2ded
Karl <karl@kejk.tech>
2022-09-06 06:21:28 +0100

    fix: heading size

--- components/PageHeader.jsx
+++ components/PageHeader.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const PageHeader = ({ children }) => {
   return (
-    <h1 className="pb-4 font-sans text-4xl font-black italic text-gray-800 dark:text-gray-200">
+    <h1 className="pb-4 font-sans text-3xl md:text-5xl font-black italic text-gray-800 dark:text-gray-200">
       {children}
     </h1>
   );
