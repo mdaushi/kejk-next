bff117a48afc24119feffab55c9518bf06873255
Karl <karl@kejk.tech>
2022-10-15 10:03:47 +0100

    fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,13 +8,11 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
       fontFamily: {
-        display: ["Archivo"],
+        display: ["Archivo", ...],
         sans: ["Inter", ...defaultTheme.fontFamily.sans],
         mono: ["JetBrains Mono", ...defaultTheme.fontFamily.mono],
       },
-    },
   },
   plugins: [],
 };
