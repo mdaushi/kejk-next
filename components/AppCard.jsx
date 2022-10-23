a982136638a8f5b2abe3460cbe21497da312c541
Karl Koch <karl@kejk.tech>
2022-10-23 11:35:46 +0100

    fix: colours

--- components/AppCard.jsx
+++ components/AppCard.jsx
@@ -5,7 +5,7 @@ import { ArrowTopRightOnSquareIcon } from "@heroicons/react/24/outline";
 const AppCard = ({ title, subtitle, image, link }) => {
   return (
     <a
-      className=" space-y-2 rounded-xl border border-neutral-200 p-4 transition-all ease-in-out hover:cursor-pointer hover:border-teal-600 hover:bg-neutral-50 dark:border-neutral-700 dark:hover:bg-neutral-900
+      className=" space-y-2 rounded-xl border border-neutral-200 p-4 transition-all ease-in-out hover:cursor-pointer hover:border-teal-600 hover:bg-neutral-50 dark:border-neutral-700 dark:hover:bg-neutral-800
     "
       href={link}
       rel="noreferrer"
