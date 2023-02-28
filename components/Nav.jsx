15695deccec7ead0b075b727f0b9486dd9bf80ff
Karl <karl@kejk.tech>
2023-02-28 21:34:15 +0000

    fix 2

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -153,7 +153,7 @@ const Nav = () => {
       </div>
       <div
         as="nav"
-        className="fixed bottom-4 z-50 mx-auto flex h-max w-[95vw] items-center justify-center rounded-full border border-neutral-200 bg-neutral-100 shadow-lg duration-500 dark:border-neutral-700 dark:bg-neutral-800 dark:shadow-none md:hidden"
+        className="fixed bottom-4 left-0 right-0 py-2 z-50 mx-auto flex h-max w-[95vw] items-center justify-center rounded-full border border-neutral-200 bg-neutral-100 shadow-lg duration-500 dark:border-neutral-700 dark:bg-neutral-800 dark:shadow-none md:hidden"
       >
         {/* Mobile nav */}
         <div className="flex items-center justify-center">
