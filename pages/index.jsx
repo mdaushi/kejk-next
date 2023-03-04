04f278315f427b8a009b088ccc6df56568390414
Karl Koch <karl@kejk.tech>
2023-03-04 11:49:05 +0000

    feat: Adds Playground for personal projects

--- pages/index.jsx
+++ pages/index.jsx
@@ -161,6 +161,26 @@ export default function Home({
             );
           })}
         </div>
+        <div className="mx-auto mt-8 flex w-full flex-col items-center justify-center space-y-4 md:flex-row md:space-x-4 md:space-y-0">
+          <Link href={"/playground"}>
+            <Button
+              bgColor="bg-neutral-100 dark:bg-neutral-800"
+              textColor="text-black dark:text-white"
+            >
+              Playground
+              <ArrowLongRightIcon className="ml-2 h-6 w-6 flex-shrink-0 text-neutral-500 dark:text-neutral-400" />
+            </Button>
+          </Link>
+          <Link href={"/projects"}>
+            <Button
+              bgColor="bg-neutral-100 dark:bg-neutral-800"
+              textColor="text-black dark:text-white"
+            >
+              Client projects
+              <ArrowLongRightIcon className="ml-2 h-6 w-6 flex-shrink-0 text-neutral-500 dark:text-neutral-400" />
+            </Button>
+          </Link>
+        </div>
         <AllCapsHeader marginTop={16} justify={"justify-start"}>
           <FlagIcon className="mr-2 h-4 w-4 flex-shrink-0 text-neutral-500 dark:text-neutral-400" />
           Recent features
