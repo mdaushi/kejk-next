6d6707e94803c8d2a8ed06865c58dc44b2f23ae8
Karl <karl@kejk.tech>
2023-03-24 08:33:52 +0000

    Refactor writing data fetching in [slug].jsx page

--- pages/thoughts/[slug].jsx
+++ pages/thoughts/[slug].jsx
@@ -223,20 +223,18 @@ export default function Post({ allPosts, post }) {
 }
 
 export async function getStaticProps({ params, preview = null }) {
-  const data = await bucket.objects
-    .findOne({
-      "type": "writings",
-      "slug": params.slug
-  }).props(["id", "slug", "content", "title", "metadata", "modified_at", "created_at",
-    preview])
-    .status("any");
+  const data = await bucket.objects.find({
+      type: "writings",
+      slug: params.slug
+  }).props("id,slug,content,title,metadata,modified_at,created_at",
+    preview).status("any");
     
   const post = await data.objects;
 
-  const allWritingData = await bucket.object.find({
-      "type": "writings"
-  }).props(["id","slug","content","title","metadata"])
-    .limit(4);
+  const allWritingData = await bucket.objects.find({
+    type: "writings"
+  }).props("slug,title,metadata").limit(4);
+
   const allPosts = await allWritingData.objects;
 
   return {
@@ -246,9 +244,11 @@ export async function getStaticProps({ params, preview = null }) {
 
 export async function getStaticPaths() {
   const data = await bucket.objects.find({
-      "type": "writings",
-  }).props(["id","slug","content","title","metadata"]);
+      type: "writings",
+  }).props("id,slug,content,title,metadata");
+  
   const allPosts = await data.objects;
+  
   return {
     paths: allPosts.map((post) => `/thoughts/${post.slug}`),
     fallback: true,
