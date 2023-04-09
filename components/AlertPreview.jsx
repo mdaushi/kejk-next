a4135d02facc90b62a93c898c5e85a31da2a0268
Karl Koch <karl@kejk.tech>
2023-04-09 19:06:31 +0100

    fix: preview alert

--- components/AlertPreview.jsx
+++ components/AlertPreview.jsx
@@ -1,10 +1,8 @@
-import Link from "next/link";
-
 const AlertPreview = () => {
   return (
-    <div className="fixed left-0 top-0 z-20 w-full bg-zinc-100 px-8 dark:bg-zinc-900 md:top-16">
+    <div className="fixed left-0 top-0 z-40 w-full bg-zinc-100 px-8 dark:bg-zinc-900 md:top-16">
       <div className="py-3 text-center text-sm font-semibold text-zinc-900 dark:text-zinc-50">
-        <>Preview mode</>
+        Preview mode
       </div>
     </div>
   );
